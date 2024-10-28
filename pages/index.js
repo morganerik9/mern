778a4093cabe4f4adbfc591f7a567c04797cc088
Daniel DeFrance <daniel.defrance@montana.edu>
2024-10-28 13:44:19 -0600

    Initial commit, changed port num

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <h1 className={styles.title}>
+          Daniel DeFrance welcomes <a href="https://nextjs.org">Next.js!</a>
+      </h1>
+        
+    </div>
+  );
+}
